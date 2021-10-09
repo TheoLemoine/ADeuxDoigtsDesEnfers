c573d2f63521271d8a73841b7d1227c72ef872b3
Basile DE LAMBERTERIE <basile.de-lamberterie@polytechnique.edu>
2021-10-09 16:02:03 +0200

    Basile - Body - Fonctionnal cutter and grower

--- Assets/Scripts/Body/MemberCollider.cs
+++ Assets/Scripts/Body/MemberCollider.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [RequireComponent(typeof(Collider2D))]
 public class MemberCollider : MonoBehaviour
 {
+    public bool isActiveByDefault;
     public Member member;
     private void OnMouseUpAsButton()
     {
