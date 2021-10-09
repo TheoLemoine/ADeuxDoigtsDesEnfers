6c6581c47feadd9ab89dee366223fd88182c2d05
Théo Lemoine <theo.lemoine0@gmail.com>
2021-10-09 22:54:08 +0200

    added narration

---
+++ Assets/Scripts/Narration.cs
@@ -0,0 +1,25 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class Narration : MonoBehaviour
+{
+    private static Narration instance;
+
+    public TextMeshPro narrationTMP;
+
+    private void Start()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("Can have more than one narration manager");
+            Destroy(this);
+        }
+                
+        instance = this;
+    }
+
+    public static void SetText(string text)
+    {
+        instance.narrationTMP.text = text;
+    }
+}
