9cd107625aa0d154c584a0532a3054e38bc8c46b
Théo Lemoine <theo.lemoine0@gmail.com>
2021-10-10 11:40:11 +0200

    Added win cards

---
+++ Assets/Scripts/ScriptableObjects/Cards/EndGame/SimpleCondition.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ScriptableObjects.Cards.EndGame
+{
+    [Serializable]
+    public struct SimpleCondition
+    {
+        public Members neededMember;
+        public int neededAmount;
+
+        public bool IsValid() => Body.memberCount(neededMember) >= neededAmount;
+    }
+}
